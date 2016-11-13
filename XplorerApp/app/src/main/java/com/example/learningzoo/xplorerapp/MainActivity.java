170c0c027c96677870cdd363d325de3d0a64649c
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-11-13 10:04:44 +0800

    seting up google map and location search

--- XplorerApp/app/src/main/java/com/example/learningzoo/xplorerapp/MainActivity.java
+++ XplorerApp/app/src/main/java/com/example/learningzoo/xplorerapp/MainActivity.java
@@ -5,13 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String[] ATTRACTIONS = new String[] {
+            "Marina Bay Sands",
+            "Singapore Flyer",
+            "Vivo City",
+            "Resort World Sentosa",
+            "Buddha Tooth Relic Temple",
+            "Singapore Zoo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, ATTRACTIONS);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView);
+        textView.setAdapter(adapter);
     }
 
     protected void toChoose(View v) {
@@ -21,5 +50,23 @@ public class MainActivity extends AppCompatActivity {
 
     protected void toExpense(View v) {
         startActivity(new Intent(this, ExpensesManagerActivity.class));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        CameraPosition singaporePos = CameraPosition.builder()
+                .target(new LatLng(1.3085486,103.8174804))
+                .zoom(10)
+                .build();
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(singaporePos), 2000, null);
+//
+//        map.addMarker(new MarkerOptions()
+//                .position(new LatLng(37.4629101,-122.2449094))
+//                .title("Facebook")
+//                .snippet("Facebook HQ: Menlo Park"))
+//                .showInfoWindow();
     }
 }
